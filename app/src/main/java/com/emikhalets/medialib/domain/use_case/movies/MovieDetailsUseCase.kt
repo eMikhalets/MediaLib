232812e202d0d91c7f4de99d80083710903cc2ec
Mikhalets Evgeny <evg.mikhalets@gmail.com>
2023-01-15 21:08:52 +0600

    fix: updating lastUpdateTime;

--- app/src/main/java/com/emikhalets/medialib/domain/use_case/movies/MovieDetailsUseCase.kt
+++ app/src/main/java/com/emikhalets/medialib/domain/use_case/movies/MovieDetailsUseCase.kt
@@ -2,6 +2,7 @@ package com.emikhalets.medialib.domain.use_case.movies
 
 import com.emikhalets.medialib.domain.entities.movies.MovieFullEntity
 import com.emikhalets.medialib.domain.repository.DatabaseRepository
+import java.util.*
 import javax.inject.Inject
 
 class MovieDetailsUseCase @Inject constructor(
@@ -13,13 +14,19 @@ class MovieDetailsUseCase @Inject constructor(
     }
 
     suspend fun updateMoviePosterUrl(posterUrl: String, entity: MovieFullEntity): Result<Unit> {
-        val newMovieEntity = entity.movieEntity.copy(poster = posterUrl)
+        val newMovieEntity = entity.movieEntity.copy(
+            poster = posterUrl,
+            lastUpdateTimestamp = Calendar.getInstance().timeInMillis
+        )
         val newEntity = entity.copy(movieEntity = newMovieEntity)
         return databaseRepository.updateMovie(newEntity)
     }
 
     suspend fun updateMovieRating(rating: Int, entity: MovieFullEntity): Result<Unit> {
-        val newMovieEntity = entity.movieEntity.copy(rating = rating)
+        val newMovieEntity = entity.movieEntity.copy(
+            rating = rating,
+            lastUpdateTimestamp = Calendar.getInstance().timeInMillis
+        )
         val newEntity = entity.copy(movieEntity = newMovieEntity)
         return databaseRepository.updateMovie(newEntity)
     }
--- app/src/main/java/com/emikhalets/medialib/domain/use_case/movies/MovieEditUseCase.kt
+++ app/src/main/java/com/emikhalets/medialib/domain/use_case/movies/MovieEditUseCase.kt
@@ -2,6 +2,7 @@ package com.emikhalets.medialib.domain.use_case.movies
 
 import com.emikhalets.medialib.domain.entities.movies.MovieFullEntity
 import com.emikhalets.medialib.domain.repository.DatabaseRepository
+import java.util.*
 import javax.inject.Inject
 
 class MovieEditUseCase @Inject constructor(
@@ -16,7 +17,10 @@ class MovieEditUseCase @Inject constructor(
         return if (entity.movieEntity.id == 0L) {
             databaseRepository.insertMovie(entity)
         } else {
-            databaseRepository.updateMovie(entity)
+            val newMovieEntity = entity.movieEntity
+                .copy(lastUpdateTimestamp = Calendar.getInstance().timeInMillis)
+            val newEntity = entity.copy(movieEntity = newMovieEntity)
+            databaseRepository.updateMovie(newEntity)
         }
     }
 }
